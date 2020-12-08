7a808fdd21a21e29c7989eca0aeba33477cc8b59
thinking <thinkingsai@gmail.com>
2020-12-08 20:21:49 +0630

    created api and service

--- src/components/content/main-content/MainContent.js
+++ src/components/content/main-content/MainContent.js
@@ -1,58 +1,96 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './MainContent.scss';
+import { connect } from 'react-redux';
 import Slideshow from '../slideshow/Slideshow';
 import Paginate from '../paginate/Paginate';
 import Grid from '../grid/Grid';
+import PropTypes from 'prop-types';
+import { IMAGE_URL } from '../../../services/movies.service';
+import { getMovies, setResponsePageNumber } from '../../../redux/actions/movies';
 
-const MainContent = () => {
-  const images = [
-    {
-      url: 'https://goldenline.bg/wp-content/uploads/2016/12/mood-girl-sand-sea-water-wave-signs-people-sun-sunset-background-wallpaper-widescreen-fullscreen-widescreen-HD-wallpapers-background.jpg',
-      rating: 7.5
-    },
-    {
-      url: 'https://static2.makeuseofimages.com/wp-content/uploads/2017/02/Photoshop-Replace-Background-Featured.jpg',
-      rating: 8.5
-    },
-    {
-      url: 'https://static2.makeuseofimages.com/wp-content/uploads/2017/02/Photoshop-Replace-Background-Featured.jpg',
-      rating: 7.8
-    },
-    {
-      url: 'https://static2.makeuseofimages.com/wp-content/uploads/2017/02/Photoshop-Replace-Background-Featured.jpg',
-      rating: 9.7
-    },
-    {
-      url: 'https://scrubbing.in/wp-content/uploads/2018/02/pexels-photo-547557.jpeg',
-      rating: 7.5
-    },
-    {
-      url: 'https://scrubbing.in/wp-content/uploads/2018/02/pexels-photo-547557.jpeg',
-      rating: 6.5
+const MainContent = (props) => {
+  const { list, movieType, totalPages, page, getMovies, setResponsePageNumber } = props;
+  const [currentPage, setCurrentPage] = useState(page);
+  const [images, setImages] = useState([]);
+  const randomMovies = list.sort(() => Math.random() - Math.random()).slice(0, 4);
+
+  const HEADER_TYPE = {
+    now_playing: 'Now Playing',
+    popular: 'Popular',
+    top_rated: 'Top Rated',
+    upcoming: 'Upcoming'
+  };
+
+  useEffect(() => {
+    if (randomMovies.length) {
+      const IMAGES = [
+        {
+          id: 1,
+          url: `${IMAGE_URL}${randomMovies[0].backdrop_path}`
+        },
+
+        {
+          id: 2,
+          url: `${IMAGE_URL}${randomMovies[1].backdrop_path}`
+        },
+
+        {
+          id: 3,
+          url: `${IMAGE_URL}${randomMovies[2].backdrop_path}`
+        },
+
+        {
+          id: 4,
+          url: `${IMAGE_URL}${randomMovies[3].backdrop_path}`
+        }
+      ];
+      setImages(IMAGES);
     }
-  ];
-  const [currentPage, setCurrentPage] = useState(1);
+  }, []);
+  useEffect(() => {
+    setCurrentPage(page);
+  }, [page, totalPages]);
 
   const paginate = (type) => {
+    let pageNumber = currentPage;
     if (type === 'prev' && currentPage >= 1) {
-      setCurrentPage((prev) => prev - 1);
+      pageNumber -= 1;
+      setCurrentPage(pageNumber);
     } else {
-      setCurrentPage((prev) => prev + 1);
+      pageNumber += 1;
     }
+    setCurrentPage(pageNumber);
+    setResponsePageNumber(pageNumber, totalPages);
+    getMovies(movieType, pageNumber);
   };
 
   return (
     <div className="main-content">
       <Slideshow images={images} auto={true} showArrows={true} />
       <div className="grid-movie-title">
-        <div className="movieType">Now Playing</div>
+        <div className="movieType">{HEADER_TYPE[movieType]}</div>
         <div className="paginate">
-          <Paginate currentPage={currentPage} totalPages={10} paginate={paginate} />
+          <Paginate currentPage={currentPage} totalPages={totalPages} paginate={paginate} />
         </div>
       </div>
-      <Grid images={images} />
+      <Grid />
     </div>
   );
 };
 
-export default MainContent;
+MainContent.propTypes = {
+  list: PropTypes.array.isRequired,
+  movieType: PropTypes.string.isRequired,
+  totalPages: PropTypes.number.isRequired,
+  page: PropTypes.number.isRequired,
+  getMovies: PropTypes.func.isRequired,
+  setResponsePageNumber: PropTypes.func.isRequired
+};
+const mapStateToProps = (state) => ({
+  list: state.movies.list,
+  movieType: state.movies.movieType,
+  totalPages: state.movies.totalPages,
+  page: state.movies.page
+});
+
+export default connect(mapStateToProps, { getMovies, setResponsePageNumber })(MainContent);
